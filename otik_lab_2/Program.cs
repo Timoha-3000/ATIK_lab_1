651e9f806cb8bd7dd36e23fbcbf51d817c9a0804
pl_konstkri <fcbsport2015@bk.ru>
2023-10-12 21:42:36 +0300

    делаем так, чтобы всё заработало, после моих фу-фу махинаций

---
+++ otik_lab_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otik_lab_2
+{
+    internal class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("hello!");
+            Console.ReadKey();
+        }
+    }
+}
